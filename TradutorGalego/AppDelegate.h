c1c53afd78b2e24e32bd3cc874bd88fcde230782
Xurxo Méndez Pérez <sonxurxo@gmail.com>
2012-01-15 17:10:58 +0100

    Initial Commit

---
+++ TradutorGalego/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TradutorGalego
+//
+//  Created by Xurxo Méndez Pérez on 15/01/12.
+//  Copyright (c) 2012 ninguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TradutorGalego/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TradutorGalego
+//
+//  Created by Xurxo Méndez Pérez on 15/01/12.
+//  Copyright (c) 2012 ninguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
